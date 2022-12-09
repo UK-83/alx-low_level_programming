1b7d6c62011659bd3c73292b54f52ec746f6156b
Ukpebor tracy <tracymarvel83@gmail.com>
2022-12-09 03:33:38 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - myc
- * Return: Always O
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
-	return (O);
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}       	
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main -  Entry point
+ *
+ * Return: Always 0  (success)
+ */
+
+int main(void)
+
+{
+	(" with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
