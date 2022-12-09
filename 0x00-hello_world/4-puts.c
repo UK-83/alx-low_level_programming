425778a2b47a34fb2a24184a3f66ca1942701bb7
Ukpebor tracy <tracymarvel83@gmail.com>
2022-12-09 00:15:44 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - myc
  * Return: Always O
  */
 int main(void)
 {
 	puts("/"Programming is like building a multilingual puzzle");
 	return (O);
+}
