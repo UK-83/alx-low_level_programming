7fd3e2b10adfae8e513d7a85e72776e96e83f2d5
Ukpebor tracy <tracymarvel83@gmail.com>
2022-12-22 06:17:52 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -21,14 +21,15 @@ char *_strcat(char *dest, char *src);
 		i++;
 	}
 	j = 0;
-	while (src[j]! = '\0')
+	while (src[j] != '\0')
 
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
+
 	}
-	dest[i]= '\0';
+	dest[i] = '\0';
 	return(dest);
 }
 
