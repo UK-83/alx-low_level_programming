97dc22491af5911e8c2b249c2fff51fbc8a5e1c4
Ukpebor tracy <tracymarvel83@gmail.com>
2022-12-22 04:57:16 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * strcat-concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src);
+
+
+{
+	int i;
+	int j;
+
+	i = o;
+
+	while (dest[i] != '\0)
+
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j]! = '\0')
+
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i]= '\0';
+	return(dest);
+}
+
+
