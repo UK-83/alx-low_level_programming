44f285f3b52a04b8a398ea07fa9279fbf54370b6
Ukpebor tracy <tracymarvel83@gmail.com>
2023-01-02 13:28:46 -0800

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+* @s: starting address of memory to be changed
+* @b: the desired value
+* @n: number of bytes to be changed
+*
+* Return:change every array with new value for a byte
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int i = 0;
+	for (; n > 0; i++)
+	{
+	s[i] = b;
+        n++;
+	}
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef  MAIN_H
+#define  MAIN_H
+
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -9,6 +10,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+
+
 #endif
-
-
